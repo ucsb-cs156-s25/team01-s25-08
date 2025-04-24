6f50a80719e553efc3852da98eed224835a882a4
giozucca <80614385+giozucca@users.noreply.github.com>
2025-04-23 18:19:47 -0700

    removed redundant imports

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+// import edu.ucsb.cs156.example.entities.UCSBDate;
 import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
 
 import org.springframework.data.repository.CrudRepository;
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 /**
  * The UCSBDateRepository is a repository for UCSBDate entities.
  */
-import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
 
 @Repository
 public interface UCSBDiningCommonsMenuItemRepository extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {

6ebaf51cd51747981a05909f25006d8f4589965c
giozucca <80614385+giozucca@users.noreply.github.com>
2025-04-24 14:55:42 -0700

    fix: correct Javadoc description for UCSBDiningCommonsMenuItem entity

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -9,11 +9,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 /**
- * This is a JPA entity that represents a UCSBDate, i.e. an entry
- * that comes from the UCSB API for academic calendar dates.
+ * This is a JPA entity that represents a UCSBDinningCommonsMenuItem, i.e. an entry
+ * that comes from the UCSB API for dining commons menu items.
  */
 
 @Data
